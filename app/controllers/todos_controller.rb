1b5a0bde29b36a11d6ca3d81c2ed3571aba24471
dirkdunn <dirkdunn1@gmail.com>
2017-05-24 23:07:17 -0500

    fix add issue

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,22 +6,34 @@ class TodosController < ApplicationController
 
   def new
     @todo = Todo.new
-    @todos = Todo.last(5).reverse
+  end
+
+  def show
+    @todo_added = Todo.find(params[:id])
   end
 
   def create
     # Using rails 4 "strong params" to whitelist only certain params for this route
     # when a request is made here, we grab name and description from params, and put them in .new
-    @todo = Todo.new(todo_params)
-    @todo.save
-    # Todo.create(name: params[:todo][:name], description: params[:todo][:description])
 
+    @todo = Todo.new(todo_params)
+    # @todo will have errors @todo.errors.full_messages if it doesnt work.
+    # Todo.create(name: params[:todo][:name], description: params[:todo][:description])
     # puts Todo.create(name: params[:todo][:name], description: params[:todo][:description])
-    redirect_to todo_path(:index)
+
+    if @todo.save
+      # Create a flash message
+      flash[:notice] = "Todo was created successfully!"
+      redirect_to todo_path(@todo)
+    else
+      puts "FAIL"
+      render "new"
+    end
+
   end
 
-  private
 
+  private
   # only permit name & description parameters during this request.
   # this is good when you want to just slap todo_params in a .update method or something,
   # and not have to worry about hackers putting in url params to screw up your db
